4ad85dd2d929a143761a400a71a1b193f42144ea
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2024-02-02 14:25:43 +0530

    made some animations

--- src/Images.js
+++ src/Images.js
@@ -3,17 +3,19 @@ import './Images.css';
 import { gsap } from 'gsap';
 
 const Images = ({ timeline, ease }) => {
-  let image1 = useRef(null);
-  let image2 = useRef(null);
-  let image3 = useRef(null);
+  const image1 = useRef(null);
+  const image2 = useRef(null);
+  const image3 = useRef(null);
 
   useEffect(() => {
+    const images = [image1.current, image2.current, image3.current];
+
     // Set initial state (visible)
-    gsap.set([image1, image2, image3], { opacity: 1, y: 0 });
-  
+    gsap.set(images, { opacity: 1, y: 0 });
+
     // Animation
     timeline.fromTo(
-      image1,
+      image1.current,
       {
         y: 1200,
         opacity: 0,
@@ -25,9 +27,9 @@ const Images = ({ timeline, ease }) => {
         duration: 1.2,
       }
     );
-  
+
     timeline.fromTo(
-      image2,
+      image2.current,
       {
         y: 1200,
         opacity: 0,
@@ -40,9 +42,9 @@ const Images = ({ timeline, ease }) => {
       },
       "-=1"
     );
-  
+
     timeline.fromTo(
-      image3,
+      image3.current,
       {
         y: 1200,
         opacity: 0,
@@ -55,16 +57,14 @@ const Images = ({ timeline, ease }) => {
       },
       "-=1"
     );
-  }, [timeline, ease, image1, image2, image3]);
-  
-  
+  }, [timeline, ease]);
 
   return (
     <div>
       <div className="images">
-        <div className="box1" ref={(el) => (image1 = el)}></div>
-        <div className="box2" ref={(el) => (image2 = el)}></div>
-        <div className="box3" ref={(el) => (image3 = el)}></div>
+        <div className="box1" ref={image1}></div>
+        <div className="box2" ref={image2}></div>
+        <div className="box3" ref={image3}></div>
       </div>
     </div>
   );
