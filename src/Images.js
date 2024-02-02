cda662506a474521354ded51e3e60d92635e0b3d
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2024-02-02 14:49:50 +0530

    mode changes in image animation

--- src/Images.js
+++ src/Images.js
@@ -1,73 +1,52 @@
-import React, { useRef, useEffect } from 'react';
-import './Images.css';
+import React, {useRef, useEffect} from 'react'
+import "./Images.css";
 import { gsap } from 'gsap';
-
-const Images = ({ timeline, ease }) => {
-  const image1 = useRef(null);
-  const image2 = useRef(null);
-  const image3 = useRef(null);
+const Images = ({timeline, ease}) => {
+  let image1 = useRef(null)
+  let image2 = useRef(null)
+  let image3 = useRef(null)
 
   useEffect(() => {
-    const images = [image1.current, image2.current, image3.current];
 
-    // Set initial state (visible)
-    gsap.set(images, { opacity: 1, y: 0 });
-
-    // Animation
-    timeline.fromTo(
-      image1.current,
-      {
-        y: 1200,
-        opacity: 0,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        ease: ease,
-        duration: 1.2,
-      }
-    );
-
-    timeline.fromTo(
-      image2.current,
-      {
-        y: 1200,
-        opacity: 0,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        ease: ease,
-        duration: 1.2,
-      },
-      "-=1"
-    );
-
-    timeline.fromTo(
-      image3.current,
-      {
-        y: 1200,
-        opacity: 0,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        ease: ease,
-        duration: 1.2,
-      },
-      "-=1"
-    );
-  }, [timeline, ease]);
-
+    gsap.set([image1, image2, image3], { opacity: 1, y: 0 });
+  
+    timeline.from(image1, 1.2, {
+      y:1200,
+      ease: ease,
+      opacity: 0
+    })
+    .from(image1, 2, {
+      scale: 1.6,
+      ease: ease,
+    }, "-=1")
+    timeline.from(image2, 1.2, {
+      y:1200,
+      ease: ease,
+      opacity: 0
+    })
+    .from(image2, 2, {
+      scale: 1.6,
+      ease: ease,
+    }, "-=1")
+    timeline.from(image3, 1.2, {
+      y:1200,
+      ease: ease,
+      opacity: 0
+    })
+    .from(image3, 2, {
+      scale: 1.6,
+      ease: ease,
+    }, "-=1")
+  }, [timeline, ease, image1, image2, image3])
   return (
     <div>
-      <div className="images">
-        <div className="box1" ref={image1}></div>
-        <div className="box2" ref={image2}></div>
-        <div className="box3" ref={image3}></div>
-      </div>
+        <div className="images">
+            <div className="box1" ref={el => image1 =el}></div>
+            <div className="box2" ref={el => image2 =el}></div>
+            <div className="box3" ref={el => image3 =el}></div>
+        </div>
     </div>
-  );
-};
+  )
+}
 
-export default Images;
+export default Images
